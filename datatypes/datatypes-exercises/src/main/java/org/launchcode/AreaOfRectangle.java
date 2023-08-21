07c1bd4b3f2c6c5292767e285f9acf932c517fcd
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-08-21 14:48:25 -0400

    Done

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AreaOfRectangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Scanner input = new Scanner(System.in);
+
         System.out.print("Please enter the length of the rectangle: ");
         int length = scanner.nextInt();
 
