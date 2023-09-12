4c25b4efab19ce02700f05d61a15a20a25206e2d
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-09-11 21:07:13 -0400

    Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -5,6 +5,63 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
+    @Test
+    public void emptyStringShouldReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void singlePairOfBracketsShouldReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void multiplePairsOfBracketsShouldReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[()]{}"));
+    }
+
+    @Test
+    public void nestedBracketsShouldReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[({})]"));
+    }
+    @Test
+    public void bracketsWithOtherCharactersShouldReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("a[b(c)d]e{f}"));
+    }
+
+    @Test
+    public void unbalancedBracketsShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void unbalancedNestedBracketsShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[({})"));
+    }
+
+    @Test
+    public void unbalancedBracketsWithOtherCharactersShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("a[b(c)d"));
+    }
+    /*@Test
+    public void bracketsInWrongOrderShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }*/
+
+    /*@Test
+    public void bracketsInWrongOrderWithOtherCharactersShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("a]b[c"));
+    }
+
+    @Test
+    public void mismatchedBracketsShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[{]}"));
+    }*/
+
+    @Test
+    public void extraClosingBracketsShouldReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[()]{}]"));
+    }
     //TODO: add tests here
     @Test
     public void emptyTest() {
