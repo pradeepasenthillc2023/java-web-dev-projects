5b2eecd33a8956d99424025616198dc401a45735
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-09-18 12:01:53 -0400

    Done

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -12,8 +12,10 @@ public class Main {
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
         flavors.sort(new FlavorComparator());
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println(flavors);
+        System.out.println(cones);
     }
-}
+    }
